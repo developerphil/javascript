3e2c6d346081743f4323f3a8256f035a4ec7b798
developerphil <phil@fullstackdevelopment.co.uk>
2015-04-23 23:02:12 +0100

    Commit

---
+++ javascript/typescript/Example/blogengine/Web/app/sitesettings/sitesettings.routes.ts
@@ -0,0 +1,37 @@
+﻿((): void => {
+    'use strict';
+
+    angular
+        .module('app.sitesettings')
+        .config([
+            '$routeProvider',
+            '$locationProvider',
+            config]);
+
+    function config(
+        $routeProvider: ng.route.IRouteProvider,
+        $locationProvider: ng.ILocationProvider): void {
+        $routeProvider
+            .when('/admin/sitesettings', {
+                templateUrl: 'app/sitesettings/sitesettings.html',
+                controller: 'app.sitesettings.SiteSettingsController',
+                controllerAs: 'vm',
+                resolve: {
+                    siteSettings: siteSettingsResolve
+                }
+            });
+    }
+
+    siteSettingsResolve.$inject = ['app.services.SiteSettingsService'];
+    function siteSettingsResolve(
+        siteSettingsService: app.services.ISiteSettingsService): ng.IPromise<app.services.ISiteSettings> {
+        return siteSettingsService.getSettings()
+            .then((siteSettings: app.services.ISiteSettings): ng.IPromise<app.services.ISiteSettings> => {
+                return siteSettingsService.getThemes()
+                    .then((themeNames: string[]): app.services.ISiteSettings => {
+                        siteSettings.availableThemeNames = themeNames;
+                        return siteSettings;
+                    });
+            });
+    }
+})(); 
