96be39776b36e00bb0872eaf5ed2733995e9cc32
developerphil <phil@fullstackdevelopment.co.uk>
2015-05-10 21:45:01 +0100

    Require

---
+++ javascript/requirejs/spec/tasksModule.spec.js
@@ -0,0 +1,37 @@
+describe("The taskModule", function () {
+
+    var injector;
+
+    beforeEach(function (done) {
+        require(["Squire"], function (Squire) {
+            injector = new Squire();
+            done();
+        });
+    });
+
+    afterEach(function () {
+        injector.remove();
+    });
+
+
+    describe("add function", function () {
+        it("calls taskRenderer.renderNew", function (done) {
+            injector.mock("renderers/taskRenderer", {
+                renderNew: function () {}
+            });
+
+            injector.mock("data/taskData", {});
+
+            injector.require(["tasks", "renderers/taskRenderer"],
+            	function (tasks, taskRenderer) {
+	            	spyOn(taskRenderer, "renderNew");
+	                tasks.add();
+	                expect(taskRenderer.renderNew).toHaveBeenCalled();
+	                done();
+	            },
+	            function (error) {
+	            	done.error(error);
+	            });
+        });
+    });
+});
---
+++ javascript/requirejs/src/js/renderers/taskRenderer.js
@@ -0,0 +1,25 @@
+define(["jquery", "hbs!templates/taskTemplate"], function ($, taskTemplate) {
+
+    function renderTasks(tasks) {
+        var elementArray = $.map(tasks, _renderTask);
+
+        $("#task-list")
+            .empty()
+            .append(elementArray);
+    }
+
+    function renderNew() {
+        var $taskList = $("#task-list");
+        $taskList.prepend(_renderTask({}));
+    }
+
+    function _renderTask(task) {
+        var $task = $(taskTemplate(task));
+        return $task;
+    }
+
+    return {
+        renderTasks: renderTasks,
+        renderNew: renderNew
+    };
+});
